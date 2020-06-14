3e03ec44c671c705232c260845be12a1954d2c19
alexpac27 <alexpac27@gmail.com>
2020-06-14 13:40:50 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+  has_many :networks, through: :shows
+
+def full_name
+    self.first_name + " " + self.last_name
+end
+
+def list_roles
+    self.characters.map{|char_obj| char_obj.name + " - " + char_obj.show.name}
+end
+
 end
