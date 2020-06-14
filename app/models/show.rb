c06b0473b4d4ad079a5a485f28593b7ec43bb320
alexpac27 <alexpac27@gmail.com>
2020-06-14 14:53:06 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,6 +4,6 @@ class Show < ActiveRecord::Base
   belongs_to :network
 
   def actors_list
-    self.characters.map{|char_obj| char_obj.actor.full_name}
+    self.actors.map{|actor_obj| actor_obj.full_name}
   end
 end
